e56783f0f6029703fb669dbdbcdb0302c99066b0
gabrielb01 <gabrie_lb23@outlook.com>
2020-08-04 13:03:07 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -3,12 +3,48 @@ let doc = document.documentElement;
 
 window.onload = () => {
 
-    document.querySelector('.top-document').addEventListener('click', () => {
-        window.scroll({
-            top:0,
-            behavior: 'smooth'  
+    let onFocusInput = (classFocus, classAlt) => {
+        document.querySelector('.' + classFocus).addEventListener('focus', () => {
+            let alt = document.querySelector('.' + classAlt);
+            alt.style.marginTop = "-6px";
+            alt.style.backgroundColor = "#fff";
+            alt.style.color = "#666";
+            alt.style.fontSize = "14px";
         });
+    }
+
+    let onBlurInput = (classFocus, classAlt) => {
+        document.querySelector('.' + classFocus).addEventListener('blur', () => {
+            if (document.querySelector('.' + classFocus).value == "") {
+                let alt = document.querySelector('.' + classAlt);
+                alt.style.marginTop = "15px";
+                alt.style.backgroundColor = "transparent";
+                alt.style.color = "#000";
+                alt.style.fontSize = "20px";
+            }
+        });
+    }
+
+
+
+document.querySelector('.top-document').addEventListener('click', () => {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
     });
+});
+
+
+onFocusInput('input-name', 'nome');
+onBlurInput('input-name', 'nome');
+
+onFocusInput('input-email', 'email');
+onBlurInput('input-email', 'email');
+
+onFocusInput('mensagem-text', 'mensagem');
+onBlurInput('mensagem-text', 'mensagem');
+
+
 
 }
 
